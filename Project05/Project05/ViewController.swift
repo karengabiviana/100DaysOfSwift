5b1fb92ec66faa7d42503ed925b19cbdac770f25
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2022-08-08 16:23:28 -0300

    Stating and reading from disk

---
+++ Project05/Project05/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Project05
+//
+//  Created by Karen Oliveira on 08/08/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var useWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        useWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return useWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = useWords[indexPath.row]
+        return cell
+    }
+
+}
+
