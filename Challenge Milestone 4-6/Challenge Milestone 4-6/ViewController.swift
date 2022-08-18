7895af3af7e2ef2fb8edfd3aead706ebc0bcb9c1
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2022-08-18 09:36:33 -0300

    Complete challenge

--- Challenge Milestone 4-6/Challenge Milestone 4-6/ViewController.swift
+++ Challenge Milestone 4-6/Challenge Milestone 4-6/ViewController.swift
@@ -26,15 +26,33 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
     }
     
     @objc func addItem() {
+        let ac = UIAlertController(title: "Enter an item:", message: nil, preferredStyle: .alert)
+        ac.addTextField()
         
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [unowned self, ac] _ in
+            let item = ac.textFields![0].text
+            self.submit(item!)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
     }
     
     @objc func clearItems() {
-        
+        items.removeAll()
+        tableView.reloadData()
+    }
+    
+    func submit(_ item: String) {
+        items.insert(item, at: 0) //add insert on array "items"
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 }
 
