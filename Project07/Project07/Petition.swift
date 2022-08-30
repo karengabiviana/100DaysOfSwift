9e7065f2d5ceebffbe2cead23e58228c5688d30d
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2022-08-30 09:14:38 -0300

    Part 1 - Setting up, Creating the basic UI and Parsing JSON

---
+++ Project07/Project07/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project07
+//
+//  Created by Karen Oliveira on 29/08/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
