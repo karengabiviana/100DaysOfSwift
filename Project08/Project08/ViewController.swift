a7682ed8be8381a10999be65ee7e1dd70f730e0b
Karen Gabrieli Viana <karengabrieli.viana@outlook.com>
2022-09-08 09:18:47 -0300

    Challenge - thin gray line around the button view

--- Project08/Project08/ViewController.swift
+++ Project08/Project08/ViewController.swift
@@ -73,6 +73,8 @@ class ViewController: UIViewController {
         
         let buttonsView = UIView()
         buttonsView.translatesAutoresizingMaskIntoConstraints = false
+       buttonsView.layer.borderWidth = 2
+       buttonsView.layer.borderColor = UIColor.lightGray.cgColor
         view.addSubview(buttonsView)
         
         NSLayoutConstraint.activate([
